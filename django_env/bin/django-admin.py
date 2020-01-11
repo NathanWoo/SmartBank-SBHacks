0ebccb9583304340040a0a6de66a3fbf0d3b4335
NathanWoo <nathanwuyang@gmail.com>
2020-01-10 22:00:22 -0800

    nw - initialization with basic Django framework

---
+++ django_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nathanwu/UCSBCode/pythonPractices/django-youtube/django_env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
